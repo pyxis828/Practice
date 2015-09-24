5d3944e97819e4eb07c13c0b7f3b942b6e71cc01
pyxis828 <kirstinwalsh0805@gmail.com>
2015-09-24 17:52:32 -0400

    practice

---
+++ boolPractice/boolPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace boolPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = false;    //declare bool Ismale and set to equal my gender (female)
+            Console.WriteLine(isMale);  //print out result
+        }
+    }
+}
---
+++ forLoopstring to int arrays/forLoopstring to int arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace forLoopstring_to_int_arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nums = { "13", "25", "4", "6", "9", "66" };
+            int length = nums.Length;
+            int[] values = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine(values[i] = int.Parse(nums[i]));
+            }
+        }
+    }
+}
