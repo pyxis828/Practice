71e5e9d00b730e47c099e428e873660490c3ba16
pyxis828 <kirstinwalsh0805@gmail.com>
2015-09-25 09:45:17 -0400

    Practice

---
+++ multiplicationTable/multiplicationTable/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiplicationTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            int rows = 5;
+            int col = 5;
+            for(int i = 1; i <= rows; i++ )
+            {
+                for(int n = 1; n <= col; n++)
+                {
+                    Console.Write((n* i) + " ");
+                  
+                }
+                Console.WriteLine();
+            
+            }
+        }
+    }
+}
