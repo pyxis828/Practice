20090171d2a081be78a215c29d84cd5f93fc477a
pyxis828 <kirstinwalsh0805@gmail.com>
2015-09-29 19:34:02 -0400

    Practice

---
+++ SimpleCalculator/Simple Calculator/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number");
+            double one = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number");
+            double two = double.Parse(Console.ReadLine());
+            Console.WriteLine("What operation do you want to perform(+, - , * , /)?");
+            char operation = char.Parse(Console.ReadLine());
+            if (operation == '+')
+            {
+                SimpleAdd(one, two);
+            }
+            if (operation == '-')
+            {
+                SimpleSubtract(one, two);
+            }
+            if (operation == '*')
+            {
+                SimpleMultiply(one, two);
+            }
+            if (operation == '/')
+            {
+                SimpleDivide(one, two);
+            }
+            else
+            {
+                Console.WriteLine("Invalid entry");
+            }
+        }
+        static void SimpleAdd(double one, double two)
+        {
+            double result = one + two;
+            Console.WriteLine(result);
+        }
+        static void SimpleSubtract(double one, double two)
+        {
+            double result = one - two;
+            Console.WriteLine(result);
+        }
+        static void SimpleMultiply(double one, double two)
+        {
+            double result = one * two;
+            Console.WriteLine(result);
+        }
+        static void SimpleDivide(double one, double two)
+        {
+            double result = one / two;
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ StreamReaderPractice/StreamReaderPractice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace StreamReaderPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader("..\\..\\Sample.txt");
+            using (reader)
+            {
+                string line = reader.ReadLine();
+                while (line != null)
+                {
+                    Console.WriteLine(line);
+                    line = reader.ReadLine();
+                }
+            }            
+        }
+    }
+}
