b64e52c9a56ee182278bc6be72a56f8d76c54ee7
pyxis828 <kirstinwalsh0805@gmail.com>
2015-09-28 21:58:17 -0400

    Practice

---
+++ ConvertStringArraytoIntArray/ConvertStringArraytoIntArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertStringArraytoIntArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        static int[] IntArrayConvert(params string[] numList)
+        {
+            int[] intList = new int[numList.Length];
+            foreach (string num in numList)
+            {
+                for (int i = 0; i < intList.Length; i++)
+                {
+                    intList[i] = int.Parse(num);
+                }
+
+            }
+            return intList;
+        }
+    }
+}
