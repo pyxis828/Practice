ec5593c8680b653a820140f74aec2d8138ff20c0
pyxis828 <kirstinwalsh0805@gmail.com>
2015-10-01 09:35:06 -0400

    Practice

---
+++ CatchExceptions/CatchExceptions/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CatchExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filename = "..\\..\\text.txt";
+            try
+            {
+                StreamReader reader = new StreamReader(filename);
+                Console.WriteLine("File " + filename + " successfully opened");
+                Console.WriteLine("File contents:");
+                using (reader)
+                {
+                    Console.WriteLine(reader.ReadLine());
+                    
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Cannot find file " + filename);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Console.WriteLine("Cannot find directory in the file path");
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("Cannot open the file " + filename);
+            }
+        }
+    }
+}
