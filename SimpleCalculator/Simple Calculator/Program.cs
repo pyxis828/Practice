1f134dd408d1a6fe1c748af1636fcf51bd265d60
pyxis828 <kirstinwalsh0805@gmail.com>
2015-11-18 11:29:04 -0500

    practice

--- SimpleCalculator/Simple Calculator/Program.cs
+++ SimpleCalculator/Simple Calculator/Program.cs
@@ -10,16 +10,16 @@ namespace Simple_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the first number");
-            double one = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the first number");                //a simple calculator which will perform basic
+            double one = double.Parse(Console.ReadLine());              //operation +, -, *, / on two numbers
             Console.WriteLine("Enter the second number");
-            double two = double.Parse(Console.ReadLine());
-            Console.WriteLine("What operation do you want to perform(+, - , * , /)?");
+            double two = double.Parse(Console.ReadLine());                              //request 2 numbers and operation to 
+            Console.WriteLine("What operation do you want to perform(+, - , * , /)?");  //perform from user
             char operation = char.Parse(Console.ReadLine());
             if (operation == '+')
-            {
-                SimpleAdd(one, two);
-            }
+            {                                                       //determine which operation was chosen, pass the numbers
+                SimpleAdd(one, two);                                //to the appropriate method
+            }   
             if (operation == '-')
             {
                 SimpleSubtract(one, two);
@@ -37,22 +37,22 @@ namespace Simple_Calculator
                 Console.WriteLine("Invalid entry");
             }
         }
-        static void SimpleAdd(double one, double two)
+        static void SimpleAdd(double one, double two)                       //add method
         {
             double result = one + two;
             Console.WriteLine(result);
         }
-        static void SimpleSubtract(double one, double two)
+        static void SimpleSubtract(double one, double two)                  //subtract method
         {
             double result = one - two;
             Console.WriteLine(result);
         }
-        static void SimpleMultiply(double one, double two)
+        static void SimpleMultiply(double one, double two)                  //multiply method
         {
             double result = one * two;
             Console.WriteLine(result);
         }
-        static void SimpleDivide(double one, double two)
+        static void SimpleDivide(double one, double two)                    //divide method
         {
             double result = one / two;
             Console.WriteLine(result);
