88659def6e3d5057358dde2e6b332dc0f161ee81
pyxis828 <kirstinwalsh0805@gmail.com>
2015-11-18 13:00:01 -0500

    practice

--- CatchExceptions/CatchExceptions/Program.cs
+++ CatchExceptions/CatchExceptions/Program.cs
@@ -11,21 +11,21 @@ namespace CatchExceptions
     {
         static void Main(string[] args)
         {
-            string filename = "..\\..\\text.txt";
-            try
-            {
+            string filename = "..\\..\\text.txt";                                   //practice with exceptions.  These are IO 
+            try                                                                     //exceptions.  Move from more specific to 
+            {                                                                       //less specific.
                 StreamReader reader = new StreamReader(filename);
-                Console.WriteLine("File " + filename + " successfully opened");
+                Console.WriteLine("File " + filename + " successfully opened");     
                 Console.WriteLine("File contents:");
-                using (reader)
-                {
-                    Console.WriteLine(reader.ReadLine());
+                using (reader)                                                      //this is the code block that could throw an 
+                {                                                                   //an exception.  It gets wrapped in a try block
+                    Console.WriteLine(reader.ReadLine());                           
                     
                 }
             }
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("Cannot find file " + filename);
+            catch (FileNotFoundException)                                           //the catch blocks catch possible exceptions that     
+            {                                                                       //could be thrown and should return a   
+                Console.WriteLine("Cannot find file " + filename);                  //helpful message to the user.
             }
             catch (DirectoryNotFoundException)
             {
